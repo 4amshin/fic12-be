1ee1177ab8adc2916d17acd51522c05655af4815
4amshin <shinmendenz@gmail.com>
2024-10-19 12:47:28 +0800

    Show Single Product API

--- app/Http/Controllers/Api/ProductApiController.php
+++ app/Http/Controllers/Api/ProductApiController.php
@@ -30,9 +30,9 @@ class ProductApiController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Product $productApi)
     {
-        //
+        return new ProductResource($productApi);
     }
 
     /**
